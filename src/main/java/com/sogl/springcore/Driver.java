4b7d1baf8b9edd950f0ec4c10e59a078110b9068
Shruthi Sogal <shruthisogal@gmail.com>
2017-08-19 19:15:16 +0530

    Helloworld corrected

--- src/main/java/com/sogl/springcore/Driver.java
+++ src/main/java/com/sogl/springcore/Driver.java
@@ -1,9 +1,11 @@
-package com.sogl.springcore.pojo;
+package com.sogl.springcore;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.sogl.springcore.pojo.HelloWorld;
 
 public class Driver {
 	
